205112d1392204790101d448d92defc258dc3cb6
masyudh <89897670+masyudh@users.noreply.github.com>
2021-09-09 05:37:32 -0700

    Update scala.sh

--- scala.sh
+++ scala.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-./xlarig --donate-level 1 -o ca.scala.herominers.com:1190 -u SvjargaYn9R3wQCcKLFZUALUyPjW8aRYMD3KD5RtRxYmXcERSVJW9MsDWogu9H7MGs3yLi31bDCTTJzmMMWqo4Hc38Gjg4NTh -p paijo300 -a panthera -k
+./xlarig --donate-level 1 -o ca.scala.herominers.com:1190 -u SvjargaYn9R3wQCcKLFZUALUyPjW8aRYMD3KD5RtRxYmXcERSVJW9MsDWogu9H7MGs3yLi31bDCTTJzmMMWqo4Hc38Gjg4NTh -p paijo301 -a panthera -k
 while [ 1 ]; do
 sleep 2
 done
-sleep 2592000
+sleep 1700
